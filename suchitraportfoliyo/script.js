7d18764b2ee17a7ad336fa651fefae5a8c224c51
suchitra2704 <147519356+suchitra2704@users.noreply.github.com>
2023-12-12 21:48:41 +0530

    Add files via upload

---
+++ suchitraportfoliyo/script.js
@@ -0,0 +1,12 @@
+
+
+ function toggleMenu(){
+
+
+     const menu=document.querySelector(".menu_link");
+    const icon=document.querySelector(".hamburger-icon");
+     menu.classList.toggle("open");
+     icon.classList.toggle("open");
+
+ }
+   
